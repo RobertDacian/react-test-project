4695ade027bbdd1f3126777d8a6d0618be683f30
RobertDacian <tennerdigitalart@gmail.com>
2023-03-04 13:38:14 -0300

    first commit

---
+++ src/components/Services.js
@@ -0,0 +1,43 @@
+import Title from './Title';
+import { servicesData } from '../data';
+import Service from './Service';
+
+const Services = () => {
+  return (
+    <section className='section services' id='services'>
+      <Title title='services' subTitle='our' />
+      <div className='section-center services-center'>
+        {servicesData.map((service) => {
+          return <Service key={service.id} {...service} />;
+        })}
+
+        {/* <article className='service'>
+          <span className='service-icon'>
+            <i className='fas fa-tree fa-fw'></i>
+          </span>
+          <div className='service-info'>
+            <h4 className='service-title'>endless hiking</h4>
+            <p className='service-text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, officia.
+            </p>
+          </div>
+        </article> */}
+
+        {/* <article className='service'>
+          <span className='service-icon'>
+            <i className='fas fa-socks fa-fw'></i>
+          </span>
+          <div className='service-info'>
+            <h4 className='service-title'>amazing comfort</h4>
+            <p className='service-text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, officia.
+            </p>
+          </div>
+        </article> */}
+      </div>
+    </section>
+  );
+};
+export default Services;
